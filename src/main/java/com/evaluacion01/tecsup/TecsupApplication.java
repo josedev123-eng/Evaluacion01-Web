c22b7799dd5a6d99dfc83fae1d708e3fb77e1a46
Jose Rojas <maxrojasrc887@gmail.com>
2026-09-09 13:50:42 -0500

    first commit

---
+++ src/main/java/com/evaluacion01/tecsup/TecsupApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion01.tecsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecsupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecsupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluacion01/tecsup/TecsupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion01.tecsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecsupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
